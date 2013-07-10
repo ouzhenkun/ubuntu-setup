6edfac56f7ffba83a878045e930b85574f1a971d
ryan <ryan.ouzhenkun@gmail.com>
2013-07-10 10:40:59 +0800

    Add ctags conf

--- link.sh
+++ link.sh
@@ -2,13 +2,14 @@
 
 SETUP=~/workspace/ubuntu-setup
 
-ln -sf $SETUP/.bash_aliases ~/.
-ln -sf $SETUP/.vimrc.local ~/.
-ln -sf $SETUP/.vimrc.bundles.local ~/.
-ln -sf $SETUP/.gitconfig ~/.
-#ln -sf $SETUP/.proxy.ini ~/.
+ln -sf $SETUP/.bash_aliases         ~/.
+ln -sf $SETUP/.vimrc.local          ~/.
+ln -sf $SETUP/.vimrc.bundles.local  ~/.
+ln -sf $SETUP/.ctags                ~/.
+ln -sf $SETUP/.gitconfig            ~/.
+#ln -sf $SETUP/.proxy.ini           ~/.
 
-ln -sf $SETUP/xmonad.hs ~/.xmonad/.
-ln -sf $SETUP/bin/* ~/bin/.
-ln -sf $SETUP/hosts /etc/.
+ln -sf $SETUP/xmonad.hs             ~/.xmonad/.
+ln -sf $SETUP/bin/*                 ~/bin/.
+ln -sf $SETUP/hosts                 /etc/.
 
